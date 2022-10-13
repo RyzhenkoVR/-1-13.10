2bacbd3ff866640d2f8693a17789e774748191ad
RyzhenkoVR <115408602+RyzhenkoVR@users.noreply.github.com>
2022-10-13 21:20:52 +0300

    Add files via upload

---
+++ Project.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	float a, b, c,v; 
+	cin >> a >> b >> c;
+	v =b* b - 4 * a *c;
+	if (v > 0) cout << 2;
+	else if (v == 0) cout << 1;
+	else cout << 0;
+}
