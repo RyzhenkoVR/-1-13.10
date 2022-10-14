e304039a0d65c3e910ce7a535eea00c1afdeddd6
RyzhenkoVR <115408602+RyzhenkoVR@users.noreply.github.com>
2022-10-14 16:50:16 +0300

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 int main()
 {
-	float a, b, c,v; 
+	float a, b, c,D; 
 	cin >> a >> b >> c;
-	v =b* b - 4 * a *c;
-	if (v > 0) cout << 2;
-	else if (v == 0) cout << 1;
+	D =b* b - 4 * a *c;
+	if (D > 0) cout << 2;
+	else if (D == 0) cout << 1;
 	else cout << 0;
 }
